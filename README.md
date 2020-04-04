09b198e45c56e73e7f3ae26d6935f7134dee0e37
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2020-04-04 23:44:13 +0530

    API setup using python package

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Plotly Stock Ticker Dashboard
 Stock ticker dashboard using plotly dash
+
--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash.dependencies import Input,Output,State
+from iexfinance.stocks import Stock,get_historical_data
+from datetime import datetime
+import os
+import projectconfig as config
 import pandas as pd
 
 app = dash.Dash()
@@ -27,7 +32,7 @@ app.layout = html.Div([
 
     html.Div([
         html.Div([
-
+            #Buffer DIV Left Side
         ],className="column is-1"),
         html.Div([
             html.Div([
@@ -47,16 +52,45 @@ app.layout = html.Div([
                 figure={
                     'data': [
                         {'x': [1, 2], 'y': [3, 1]}
-                    ]
+                    ],
+                    'layout':{
+                        'title':'ticker',
+                        'plot_bgcolor': '#CDE0CA',
+                        'paper_bgcolor': '#CDE0CA',
+                    }
                 }
             )
         ],className="column is-7"),
         html.Div([
-
+            #Buffer DIV Right Side
         ], className="column is-1")
     ],className="columns is-vcentered")
     #END OF ROW1
 ],className="has-background-grey-darker")
+
+
+@app.callback(Output('my_graph','figure'),
+              [Input('my_ticker_symbol','value')])
+def update_chart(stock_ticker):
+    start = datetime(2016, 9, 1)
+    end = datetime(2018, 9, 1)
+    print(os.getenv('IEX_TOKEN'))
+    df = get_historical_data(stock_ticker, start, end, output_format='pandas', token=config.api_key)
+    print(df)
+    figure = {
+        'data': [
+            {'x': df.index, 'y': df.close}
+        ],
+        'layout': {
+            'title': stock_ticker,
+            'plot_bgcolor': '#CDE0CA',
+            'paper_bgcolor': '#BFD8FF',
+        }
+    }
+
+    return figure
+
+
 
 if __name__ == "__main__":
     app.run_server()
