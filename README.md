719eb5f07c0b6f05958492f4356da77a7ca525b5
Vasav Anandjiwala <vasav.anandjiwala@gmail.com>
2020-04-05 14:13:29 +0530

    Datepicker callback implementation. Updated TODO list and execution instructions in readme.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Plotly Stock Ticker Dashboard
 Stock ticker dashboard using plotly dash
 
+##Instruction
+1. Rename projectconfig_sample.py file to projectconfig.py
+2. Obtain API key from IEX cloud website
+3. Use that key in the projectconfig.py file
+4. Use requirements.txt to install required packages 
+5. Run the server
+
+##TODO:
+* Accidently I used all API requests allowed for a month while testing. So full functionality testing is yet to be completed. 
+* Implement theame chaging (Light and Dark)
+* Watchlist implementation 
--- app.py
+++ app.py
@@ -4,11 +4,23 @@ import dash_core_components as dcc
 from dash.dependencies import Input,Output,State
 from iexfinance.stocks import Stock,get_historical_data
 from datetime import datetime
-import os
+
 import projectconfig as config
 import pandas as pd
 
 app = dash.Dash()
+
+nsdq = pd.read_csv('Data/nasdaq-company-list.csv')
+nsdq.set_index('Symbol',inplace=True)
+
+options = []
+for tic in nsdq.index:
+    stock_option = {
+        'label': '{}:{}'.format(tic,nsdq.loc[tic]['Name']),
+        'value': tic
+    }
+    options.append(stock_option)
+
 
 app.layout = html.Div([
     html.Nav([
@@ -38,10 +50,12 @@ app.layout = html.Div([
             html.Div([
                 html.Label(children="Ticker Name",className="label has-text-white"),
                 html.Div([
-                    dcc.Input(
+                    dcc.Dropdown(
                         id ='my_ticker_symbol',
-                        value='TSLA',
-                        className="input"
+                        options=options,
+                        value=['TSLA'],
+                        multi=True,
+                        style={'border-radius':0}
                     )
                 ],className="control"),
             ],className="field"),
@@ -52,14 +66,11 @@ app.layout = html.Div([
                     min_date_allowed=datetime(2020, 1, 1),
                     max_date_allowed=datetime.today(),
                     start_date=datetime(2020, 1, 1),
-                    end_date=datetime.today()
+                    end_date=datetime(2020, 1, 1)
                 )
             ],className="field"),
             html.Div([
                 html.Button('Submit', id='submit-val', n_clicks=0, className="button is-medium is-light is-fullwidth")
-            ], className="field"),
-            html.Div([
-                html.Button('Watch-List', id='submit-watchlist', n_clicks=0, className="button is-medium is-light is-fullwidth")
             ], className="field")
         ],className="column is-3"),
         html.Div([
@@ -86,33 +97,29 @@ app.layout = html.Div([
 
 
 @app.callback(Output('my_graph','figure'),
-              [Input('my_ticker_symbol','value')])
-def update_chart(stock_ticker):
-    start = datetime(2019,1,1)
-    end = datetime(2019,12,31)
+              [Input('submit-val','n_clicks')],
+              [State('my_ticker_symbol','value'),
+               State('my_date_picker', 'start_date'),
+               State('my_date_picker', 'end_date')])
+def update_chart(n_clicks,stock_ticker, start_date, end_date):
 
-    #Commented to avoid unwanted API calls while development since there is a limit to number of calls
-    #df = get_historical_data(stock_ticker, start, end, output_format='pandas', token=config.api_key, close_only=True)
-    #print(df)
-    # figure = {
-    #     'data': [
-    #         {'x': df.index, 'y': df.close}
-    #     ],
-    #     'layout': {
-    #         'title': stock_ticker,
-    #         'plot_bgcolor': '#CDE0CA',
-    #         'paper_bgcolor': '#BFD8FF',
-    #     }
-    # }
+    start = datetime.strptime(start_date[:10],'%Y-%m-%d')
+    end = datetime.strptime(end_date[:10],'%Y-%m-%d')
+
+    #TODO: Complete testing this functionality. Free tier API requrests used while testing so can not perform further testing till next month!!
+    #TODO: Resolve and handle issues around API calls
+    traces = []
+    for tic in stock_ticker:
+        df = get_historical_data(tic, start, end, output_format='pandas', token=config.api_key, close_only=True)
+        traces.append({'x':df.index,'y': df.close,'name':tic})
+
 
     figure = {
-        'data': [
-            {'x': [1, 2], 'y': [3, 1]}
-        ],
+        'data': traces,
         'layout': {
-            'title': 'ticker',
+            'title': stock_ticker,
             'plot_bgcolor': '#CDE0CA',
-            'paper_bgcolor': '#CDE0CA',
+            'paper_bgcolor': '#BFD8FF',
         }
     }
 
